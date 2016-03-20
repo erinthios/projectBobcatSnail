0a33e1ab9cefe84f617fe82e3bde52cb7b525593
Tim Miller <innerlogic4321@gmail.com>
2016-03-20 11:56:14 -0400

    Quiz Blitz Version 1

--- quizblitz.js
+++ quizblitz.js
@@ -27,6 +27,9 @@ var isCalled = false;
 	    return value.Difficulty == "1" || value.Difficulty == "2";
 	}
 var $quizblitz = {
+	questions: null,
+	currentQuestionNumber: 0,
+	allQuestions: null,
     loadJSON: function(path, callback) {
         var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
@@ -38,28 +41,78 @@ var $quizblitz = {
         };
         xobj.send(null);  
     },
+	resetGameBoard: function() {
+		$('.answer').removeClass("show-wrong");
+        $('.answer').removeClass("show-correct");
+		$('.question-bubble').removeClass("show-wrong");
+        $('.question-bubble').removeClass("show-correct");
+		for(var i = 0; i <= 3; i++) {
+			$('#question-point-total-' + i).text('');
+		}
+	},
+	startNewGame: function() {
+		$quizblitz.resetGameBoard();
+		$quizblitz.currentQuestionNumber = 0;
+		$quizblitz.loadQuestion(0, $quizblitz.questions[0]);
+		$('#title-screen').hide();
+        $('#question-template').show();
+	},
+	rerollQuestion: function() {
+		shuffle($quizblitz.allQuestions);
+		$quizblitz.questions[$quizblitz.currentQuestionNumber] = $quizblitz.allQuestions[0];
+		$quizblitz.loadQuestion($quizblitz.currentQuestionNumber, $quizblitz.questions[$quizblitz.currentQuestionNumber]);
+	},
+	nextQuestion: function() {
+		if($quizblitz.currentQuestionNumber >= 3) {
+			$quizblitz.currentQuestionNumber = 3;
+			return;
+		}
+		$('.answer').removeClass("show-wrong");
+        $('.answer').removeClass("show-correct");
+		$quizblitz.currentQuestionNumber++;
+		$quizblitz.loadQuestion($quizblitz.currentQuestionNumber, $quizblitz.questions[$quizblitz.currentQuestionNumber]);
+	},
+	previousQuestion: function() {
+		if($quizblitz.currentQuestionNumber <= 0) {
+			$quizblitz.currentQuestionNumber = 0;
+			return;
+		}
+		$('.answer').removeClass("show-wrong");
+        $('.answer').removeClass("show-correct");
+		$quizblitz.currentQuestionNumber--;
+		$quizblitz.loadQuestion($quizblitz.currentQuestionNumber, $quizblitz.questions[$quizblitz.currentQuestionNumber]);
+	},
     loadNewGame: function (path) {
-        if (!isCalled) {
-            isCalled = true;
-            $quizblitz.loadJSON(path, function (response) {
+		$quizblitz.resetGameBoard();
+		$quizblitz.currentQuestionNumber = 0;
+		if(!isCalled) {
+			isCalled = true;
+			
+			$(".answer").click(function () {
+                    var correct = $(this).attr("data-correct") === "true";
+                    $quizblitz.isCorrect(correct, false);
+                    cdreset();
+                });
+		}
+		
+		$quizblitz.loadJSON(path, function (response) {
                 var first_json = JSON.parse(response);
                 var actual_JSON = first_json.filter(HasNotBeenSeen);
-                var diffOneOrTwo = actual_JSON.filter(DiffOneOrTwo);
+                $quizblitz.allQuestions = actual_JSON.filter(DiffOneOrTwo);
 
-                $('#startGame').fadeOut();
-                $('#pointLayout').fadeIn();
-				
+                $('#title-screen').show();
+				$('#question-template').hide();
+				$('.main-content').show();
 				// shuffle question arrays
-				shuffle(diffOneOrTwo);
+				shuffle($quizblitz.allQuestions);
                 
-                var questions = [];
-				questions.push(diffOneOrTwo[0]);
-				questions.push(diffOneOrTwo[1]);
-                questions.push(diffOneOrTwo[2]);
-				questions.push(diffOneOrTwo[0]);
+                $quizblitz.questions = [];
+				$quizblitz.questions.push($quizblitz.allQuestions[0]);
+				$quizblitz.questions.push($quizblitz.allQuestions[1]);
+                $quizblitz.questions.push($quizblitz.allQuestions[2]);
+				$quizblitz.questions.push($quizblitz.allQuestions[3]);
                 
             });
-        }
     },
 	// based on https://github.com/nbrunt/TextFit
 	// modified to require a max-height
@@ -75,14 +128,18 @@ var $quizblitz = {
         $('[data-correct="false"]').addClass("show-wrong");
         $('[data-correct="true"]').addClass("show-correct");
         if (correct) {
+			$('#question-bubble-' + $quizblitz.currentQuestionNumber).toggleClass("show-correct");
             $('#right-sound').clone()[0].play();
-            $('.right-answer-choice').fadeIn();
+			$('#question-point-total-' + $quizblitz.currentQuestionNumber).text('+5');
+            //$('.right-answer-choice').fadeIn();
         } else {
             $('#wrong-sound').clone()[0].play();
-            $('.wrong-answer-choice').fadeIn();
+			$('#question-bubble-' + $quizblitz.currentQuestionNumber).toggleClass("show-wrong");
+			$('#question-point-total-' + $quizblitz.currentQuestionNumber).text('-5');
+            //$('.wrong-answer-choice').fadeIn();
         }
 
-        setTimeout(function () { $quizblitz.setAnswerClearAndReturn(correct) }, 2000);
+        //setTimeout(function () { $quizblitz.setAnswerClearAndReturn(correct) }, 2000);
     },
     loadQuestion: function (questionNumber, question) {
         $('#pointTable').fadeOut();
@@ -95,8 +152,8 @@ var $quizblitz = {
             $('#submitter-text').text(question.Submitter);
         }
 
+		$('#question-title').text(question.QKEY + ': ' + question.GameTitle);
         $('#question-text').text(question.Question);
-        $('#question-number').text(question.QKEY);
         $('#question-comment').text(question.Comments);
 
         var randomCorrectAnswerBank = Math.floor(Math.random() * (4 - 1) + 1);
@@ -119,17 +176,17 @@ var $quizblitz = {
         $("#" + wrongAnswerBanks[2]).attr("data-correct", false);
         $("#" + wrongAnswerBanks[2]).text(question.FakeAnswer3);
 
-        $('#question').delay(800).fadeIn();
+        // $('#question').delay(800).fadeIn();
 
-		// after fadeIn starts, fit text
-		setTimeout(function()
-		{
-			$quizblitz.textfit($('#question-text'));
-			$quizblitz.textfit($('#1'));
-			$quizblitz.textfit($('#2'));
-			$quizblitz.textfit($('#3'));
-			$quizblitz.textfit($('#4'));
-		}, 800);
+		// // after fadeIn starts, fit text
+		// setTimeout(function()
+		// {
+			// $quizblitz.textfit($('#question-text'));
+			// $quizblitz.textfit($('#1'));
+			// $quizblitz.textfit($('#2'));
+			// $quizblitz.textfit($('#3'));
+			// $quizblitz.textfit($('#4'));
+		// }, 800);
     },
     setAnswerClearAndReturn: function (correct) {
         $('td[data-question-number=' + $('#question').attr("data-selected-question") + ']').removeClass('answered-right');
